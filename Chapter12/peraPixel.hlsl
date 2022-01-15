056710cd52b1440a0661b5d84e9f470b816202ec
SSK-0423 <83057130+SSK-0423@users.noreply.github.com>
2022-01-15 14:04:54 +0900

    Update peraPixel.hlsl

--- Chapter12/peraPixel.hlsl
+++ Chapter12/peraPixel.hlsl
@@ -11,15 +11,15 @@ float4 ps(Output input) : SV_TARGET
     float dy = 1.f / h; //1ピクセル当たりの移動幅
     float4 ret = float4(0, 0, 0, 0);
     
-    ret += tex.Sample(smp, input.uv + float2(-128 * dx, -1 * dy));    //左上
+    ret += tex.Sample(smp, input.uv + float2(-128 * dx, -2 * dy));    //左上
     ret += tex.Sample(smp, input.uv + float2(0, -128 * dy));          //上
-    ret += tex.Sample(smp, input.uv + float2(128 * dx, -1 * dy));     //右上
+    ret += tex.Sample(smp, input.uv + float2(128 * dx, -2 * dy));     //右上
     ret += tex.Sample(smp, input.uv + float2(-128 * dx, 0));          //左
     ret += tex.Sample(smp, input.uv + float2(0,0));                 //自分
     ret += tex.Sample(smp, input.uv + float2(128 * dx, 0));           //右
-    ret += tex.Sample(smp, input.uv + float2(-128 * dx, 1 * dy));     //左下
+    ret += tex.Sample(smp, input.uv + float2(-128 * dx, 2 * dy));     //左下
     ret += tex.Sample(smp, input.uv + float2(0, 128 * dy));           //下
-    ret += tex.Sample(smp, input.uv + float2(128 * dx, 1 * dy));      //右下
+    ret += tex.Sample(smp, input.uv + float2(128 * dx, 2 * dy));      //右下
     
     ret /= 9.f;
     //return float4(float3(0.7f,0.8f,0.7f) - ret.rgb - fmod(ret.rbg, 0.25f), ret.a);
